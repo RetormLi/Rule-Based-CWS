56f58561238506b0bb25a4724e0675c7ca8305a1
RetormLi <1197334522@qq.com>
2020-09-23 20:50:23 +0800

    Improve on escape word list. Performance 0.8896.

--- data_process.py
+++ data_process.py
@@ -24,11 +24,11 @@ class Data:
                 json.dump(vocab, json_file, ensure_ascii=False)
         return vocab
 
-    def zh_token2vocab(self, save_path=None):
+    def zh_token2vocab(self, save_path=None, encoding='utf-8'):
         max_length = 0
         vocab = dict()
         cop = re.compile("[^\u4e00-\u9fa5 ]+")
-        with open(self.data_path, 'r', encoding='utf-8') as data_file:
+        with open(self.data_path, 'r', encoding=encoding) as data_file:
             for line in data_file:
                 new_line = cop.sub('', line)
                 for word in new_line.split():
@@ -40,21 +40,6 @@ class Data:
                 json.dump(vocab, json_file, ensure_ascii=False)
         print(max_length)
         return vocab
-
-    def get_special_vocab(self, save_path=None):
-        """
-        TODO
-        :param save_path:
-        :return:
-        """
-        special_vocab = dict()
-        cop = re.compile("[^\u4e00-\u9fa5^a-z^A-Z^0-9]")
-        with open(self.data_path, 'r', encoding='utf-8') as data_file:
-            pass
-        if save_path is not None:
-            with open(save_path, 'w', encoding='utf-8') as json_file:
-                json.dump(special_vocab, json_file, ensure_ascii=False)
-        return special_vocab
 
     def get_line(self, split=True):
         with open(self.data_path, 'r', encoding='utf-8') as data_file:
@@ -90,3 +75,35 @@ class Vocab:
 def get_vocab(path):
     with open(path, "r", encoding='utf-8') as json_file:
         return json.load(json_file)
+
+
+def store_vocab(path, vocab):
+    with open(path, 'w', encoding='utf-8') as json_file:
+        json.dump(vocab, json_file, ensure_ascii=False)
+
+
+def generate_vocab(origin_path, save_path):
+    vocab = dict()
+    with open(origin_path, 'r', encoding='utf-8') as origin_file:
+        for line in origin_file:
+            # word = line[:line.find('\t')]
+            word = line
+            vocab[word] = vocab.get(word, 0) + 1
+    with open(save_path, 'w', encoding='utf-8') as save_file:
+        json.dump(vocab, save_file, ensure_ascii=False)
+
+
+# escape = ['一次', '这个', '那个', '这次', '那次', '此次', '最低',
+#           '最高', '最多', '最少', '最佳', '最好', '最新', '就是',
+#           '都是', '一天']
+escape = ['一次', '这个', '那个', '这次', '那次', '此次', '最低',
+          '最高', '最多', '最少', '最佳', '最好', '最新', '就是',
+          '都是', '一天']
+if __name__ == '__main__':
+    # generate_vocab('data/THUOCL_diming.txt', 'vocab/diming_vocab.json')
+    # generate_vocab('data/THUOCL_chengyu.txt', 'vocab/chengyu_vocab.json')
+    # generate_vocab('data/THUOCL_caijing.txt', 'vocab/caijing_vocab.json')
+    # generate_vocab('data/THUOCL_food.txt', 'vocab/food_vocab.json')
+    # pku = Data('data/Chinese_Names_Corpus（120W）.txt')
+    # pku.zh_token2vocab('vocab/ch_name_vocab.json')
+    store_vocab('vocab/escape_vocab.json', dict(zip(escape, [1 for x in escape])))
--- main.py
+++ main.py
@@ -5,9 +5,24 @@ import preprocess
 import analyze
 
 if __name__ == "__main__":
-    # train = data_process.Data('data/train.txt')
-    # vocab = train.zh_token2vocab('zh_vocab_dict.json')
-    vocab = data_process.get_vocab('zh_vocab_dict.json')
+    vocab = dict()
+    vocab_paths = ['vocab/zh_vocab_dict.json',
+                   'vocab/caijing_vocab.json',
+                   'vocab/food_vocab.json',
+                   'vocab/chengyu_vocab.json',
+                   'vocab/diming_vocab.json',
+                   'vocab/pku_vocab.json',
+                   'vocab/eng_name_vocab.json']
+
+    escape_path = 'vocab/escape_vocab.json'
+    escape_vocab = data_process.get_vocab(escape_path)
+    for path in vocab_paths:
+        vocab.update(data_process.get_vocab(path))
+    for escape_word in escape_vocab:
+        if escape_word in vocab:
+            del vocab[escape_word]
+    data_process.store_vocab('vocab/big_vocab_dict.json', vocab)
+
     with open('data/test.txt', 'r', encoding='utf-8') as test_file:
         with open('181220032.txt', 'w', encoding='utf-8') as result_file:
             for line in test_file:
