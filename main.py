9de49a2e5a168074279cfc14a1bc188eed9ce036
RetormLi <1197334522@qq.com>
2020-09-23 12:50:30 +0800

    Improve on regex. Performance 0.873.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     # vocab = train.zh_token2vocab('zh_vocab_dict.json')
     vocab = data_process.get_vocab('zh_vocab_dict.json')
     with open('data/test.txt', 'r', encoding='utf-8') as test_file:
-        with open('result.txt', 'w', encoding='utf-8') as result_file:
+        with open('181220032.txt', 'w', encoding='utf-8') as result_file:
             for line in test_file:
                 preprocess_sentence, matched = preprocess.preprocess(line)
                 split_sentence = analyze.analyze(preprocess_sentence, matched, vocab)
--- preprocess.py
+++ preprocess.py
@@ -4,14 +4,16 @@ import re
 from typing import Set, AnyStr, Pattern
 
 # class Preprocesser:
-spliter = re.compile(r"[，！。？《》,@“”（）【】、‘’：；↓#:;()]|…{2}|—{2}|/{2}|/", re.UNICODE)
 zh_eng_num = re.compile(r"[\u4e00-\u9fa5a-zA-Z0-9]", re.UNICODE)
 zh_detail = re.compile(r"([\u4E00-\u9FD5]+)", re.UNICODE)
 skip_detail = re.compile(r"([.0-9]+|[a-zA-Z0-9]+)")
 eng = re.compile(r"[a-zA-Z0-9]+")
 num = re.compile("[.0-9]+")
+
 count = re.compile(r"[0-9]+/[0-9]+|[第]*[.0-9]+[%]*[余多]*[十百千万亿]*[余多]*", re.UNICODE)
-time = re.compile(r"[0-9]+[年月日][代]*", re.UNICODE)
+time = re.compile(r"[0-9]+[月日时][早晚]?|[0-9]{4}年|[0-9]+年代|[一二三四五六七八九]+十年代|[0-9]+[分点](?!钟)|(?<![0-9])[0-9]{2}后",
+                  re.UNICODE)
+spliter = re.compile(r"[，！。？《》,@“”（）【】、‘’：；↓#:;()]|…{1,2}|—{1,2}|/{1,2}|~+|\.+", re.UNICODE)
 
 # time -> count -> spliter
 patterns = [time, count, spliter]
@@ -44,8 +46,8 @@ def preprocess(sentence: AnyStr):
     return sentence.split(), matches
 
 
-# if __name__ == '__main__':
-#     line = "//你好，你是谁？我知道了。再见！现在是10月。我有0.3万元。大概1/3在银行，存了20年。100元/股。"
-#     # line = "苏更透露，10月20日印尼新总统就职当天，印尼政府将公布经济新战略，其中包括长期投资额高达7千万亿印尼盾（约合3.6万亿人民币）的基础设施建设计划。"
-#
-#     print(preprocess(line))
+if __name__ == '__main__':
+    # line = "//你好，你是谁？—我知道了。再见！现在是10月。我有0.3万元。大概1/3在银行，存了20年。100元/股。2020年，10分钟之前，12点13分的时候，20年后，90年代，二十年代。100多年来。20日早。"
+    # line = "苏更透露，10月20日印尼新总统就职当天，印尼政府将公布经济新战略，其中包括长期投资额高达7千万亿印尼盾（约合3.6万亿人民币）的基础设施建设计划。"
+    line = "转发，—周知~~~（人民网）"
+    print(preprocess(line)[0])
